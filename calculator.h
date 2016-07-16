79d9f2173b76c07f0157bee3fbde01193113b898
git <git@MacBook-Pro-anatoliykant.local>
2016-07-16 16:54:21 +0300

    ParsingSTRKant

--- calculator.h
+++ calculator.h
@@ -44,10 +44,12 @@ private:
     double mult(const double left_param, const double right_param);
     
     double sum(double a, double b);
-     double div(const double numerator, const double denominator); 
+    double div(const double numerator, const double denominator);
     
     double modulo(const double x, const double y);
     int modulo(const int x, const int y);
+    std::string expr(std::string str);
+    
       // TO DO
       // .. add something
 };
